af456479bb3bdaa33383a91e59ecf4f12bafb37c
eaguilar-git <aguilared.96@gmail.com>
2025-03-19 19:42:18 -0400

    class

---
+++ packages.R
@@ -0,0 +1,10 @@
+# Today we are going to create two sets of slide presentations
+
+# First install the following packages: rmdformats, xaringan, and cairo
+install.packages("xaringan")
+install.packages("rmdformats")
+install.packages("Cairo")
+
+#+ First version: Beamer Presentation
+#+ This is a common style used by economists
+#+ 
